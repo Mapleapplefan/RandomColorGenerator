339a9a6cad544cc00720ac4e856e112584b757db
Mapleapplefan <Mapleapplefan@github.com>
2025-08-11 12:46:48 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 
 const button = document.querySelector('button');
+const code= document.querySelector('#color-code');
 button.addEventListener('click', () => {
     document.body.style.backgroundColor = 'white';
-    const r = Math.random() * 255;
-    const g = Math.random() * 255;
-    const b = Math.random() * 255;
+    code.innerText = 'white';
+    const r = Math.floor(Math.random() * 255);
+    const g = Math.floor(Math.random() * 255);
+    const b = Math.floor(Math.random() * 255);
     document.body.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
-    h1.innerText = "rgb(${r}, ${g}, ${b})";
+    code.innerText = `rgb (${r}, ${g}, ${b})`;
 })
