f910f9a880917c316d80e4b2b5892d9af2e49ca6
Mapleapplefan <Mapleapplefan@github.com>
2025-08-11 11:30:22 -0700

    added

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 
+const button = document.querySelector('button');
+button.addEventListener('click', () => {
+    document.body.style.backgroundColor = 'white';
+    const r = Math.random() * 255;
+    const g = Math.random() * 255;
+    const b = Math.random() * 255;
+    document.body.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+    h1.innerText = "rgb(${r}, ${g}, ${b})";
+})
