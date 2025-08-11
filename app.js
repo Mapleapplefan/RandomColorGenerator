b4633ed7e35ae957e9a5509f57dd9ec20cf75ea9
Mapleapplefan <Mapleapplefan@github.com>
2025-08-11 12:52:55 -0700

    starting off with rgb white

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const code= document.querySelector('#color-code');
 button.addEventListener('click', () => {
     document.body.style.backgroundColor = 'white';
     code.style.color = 'black';
-    code.innerText = 'white';
     const r = Math.floor(Math.random() * 255);
     const g = Math.floor(Math.random() * 255);
     const b = Math.floor(Math.random() * 255);
